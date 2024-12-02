6daf641740e1e3b45f5abecc8562451f54410d06
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2024-12-03 00:36:54 +0100

    done day 1 part 1

--- App/Day1/Process.fs
+++ App/Day1/Process.fs
@@ -7,20 +7,20 @@ let run () =
     let data = getInput()
     let unfilteredData = data.Split([| ' '; '\n' |])
     let filteredData = unfilteredData |> Array.filter(fun number -> number <> "")
-    let firstList = filteredData |> Array.filter(fun number -> number |> )
     
-    for e in filteredData do
-        printfn $"%A{e}"
+    let splitArray (arr: string[]) =
+        let firstArray = [| for i in 0 .. 2 .. arr.Length - 1 -> arr.[i] |]
+        let secondArray = [| for i in 1 .. 2 .. arr.Length - 1 -> arr.[i] |]
+        (firstArray, secondArray)
 
+    let (firstList, secondList) = splitArray filteredData
+    
+    let firstListSorted = Array.sort firstList
+    let secondListSorted = Array.sort secondList
+    
+    let firstIntListSorted = Array.map int firstListSorted 
+    let secondIntListSorted = Array.map int secondListSorted
+    
+    let diffArray = Array.map2 (fun first second -> abs (first - second)) firstIntListSorted secondIntListSorted
 
-let getListFromCombined (order: String, list: Array ) =
-    match order with
-    | "first" ->
-        let mutable firstList = []
-        let mutable count = 0
-        for item in list do
-            count++
-            
-    | "second" ->
-        
-    | _ ->
+    Array.sum diffArray
